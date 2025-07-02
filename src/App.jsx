3eb11d8b2158025dd1a39d828a202ab2fe888f79
Tsering Wolfe <tseringwolfe03@gmail.com>
2025-07-02 16:31:57 -0700

    added dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Container from '@mui/material/Container'
+import { MenuItem, FormControl, InputLabel, Select } from '@mui/material';
+
+const API_KEY = import.meta.env.TMDB_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [genre, setGenre] = useState('');
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container sx={{ minWidth: 400 }}>
+
+        <FormControl fullWidth>
+          <InputLabel id="genre-label">Genre</InputLabel>
+          <Select
+            labelId="genre-label"
+            id="genre-simple-select"
+            value={genre}
+            label="Genre"
+            onChange={(event) => {
+    setGenre(event.target.value)}}
+          >
+            <MenuItem value={1}>Action</MenuItem>
+            <MenuItem value={2}>Drama</MenuItem>
+            <MenuItem value={3}>Comedy</MenuItem>
+            <MenuItem value={4}>Romance</MenuItem>
+          </Select>
+        </FormControl>
+
+      </Container>
     </>
   )
 }
