8945af58658b432260dd82939ceabcc36f0c39c3
Tsering Wolfe <tseringwolfe03@gmail.com>
2025-07-03 15:32:05 -0700

    set up dropdown menu implementation with api and make movieArray

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Container from '@mui/material/Container';
-import { MenuItem, FormControl, InputLabel, Select, createTheme, ThemeProvider, ButtonGroup, Button } from '@mui/material';
+import { createTheme, ThemeProvider, InputLabel, Select, ButtonGroup, Button, MenuItem, FormControl, Box } from '@mui/material';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 
-const API_KEY = import.meta.env.TMDB_API_KEY;
 
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
 
 function App() {
+
   const [genre, setGenre] = useState('');
 
   const themeHeader = createTheme({
@@ -29,6 +33,55 @@ function App() {
     },
   });
 
+  const [genreArray, setGenreArray] = useState([]);
+
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow"
+  };
+
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_KEY, requestOptions)
+      .then((response) => response.json())
+      .then((genreData) => {
+        console.log("got data", genreData.genres);
+        setGenreArray(genreData.genres);
+      })
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsVisible(true);
+  };
+
+  const [movieArray, setMovieArray] = useState([]);
+
+  const handleGenreChange = () => {
+
+    let genreID = ""
+
+    for (let i = 0; i < genreArray.length; i++) {
+      if (genreArray[i].name == genre) {
+        genreID = genreArray[i].id.toString();
+      }
+    }
+
+    let pageNum = getRandomInt(300).toString();
+
+    fetch("https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY + "&primary_release_date.gte=1990-01-01&with_original_language=en&with_genres=" + genreID + "&page=" + pageNum, requestOptions)
+      .then(response => response.json())
+      .then(movieData => {
+        setMovieArray(movieData.results);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+
+  console.log(movieArray);
+
   return (
     <>
       <Container fixed>
@@ -48,15 +101,12 @@ function App() {
             variant="body1"
             align="center"
             color="text.primary"
-            sx={{ mb: 6 }}
+            sx={{ mb: 2 }}
           >
             Please select a genre to begin!
           </Typography>
         </ThemeProvider>
       </Container>
-
-
-
 
       <Container fixed>
 
@@ -68,26 +118,30 @@ function App() {
               id="genre-simple-select"
               value={genre}
               label="Genre"
-              sx={{width: 200}}
+              sx={{ width: 200 }}
               onChange={(event) => {
-      setGenre(event.target.value)}}
+                setGenre(event.target.value);
+                handleGenreChange();
+              }}
             >
-              <MenuItem value={1}>Action</MenuItem>
-              <MenuItem value={2}>Drama</MenuItem>
-              <MenuItem value={3}>Comedy</MenuItem>
-              <MenuItem value={4}>Romance</MenuItem>
+              {genreArray && genreArray.map((genre) => (
+
+                <MenuItem value={genre.id}>{genre.name}</MenuItem>
+
+              ))}
             </Select>
-            <Button sx={{ bgcolor: "#898AC4" }}>
+            <Button onClick={handleButtonClick} sx={{ bgcolor: "#898AC4" }}>
               Start
             </Button>
           </ButtonGroup>
         </FormControl>
 
       </Container>
-
-      <Container fixed>
-
-      </Container>
+      {isVisible && (
+        <Typography>
+          Movie Poster here
+        </Typography>
+      )}
     </>
   )
 }
